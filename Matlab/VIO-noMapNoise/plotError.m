d2b56dded68f203ebafcb3f13fb813b6f27a13f2
weixinwang <weixinwang442@gmail.com>
2022-06-26 22:45:20 -0400

    pose estimation without map noises

---
+++ Matlab/VIO-noMapNoise/plotError.m
@@ -0,0 +1,55 @@
+clear;
+close all;
+
+load('date-largeInitErr.mat');
+t = 0:1/200:40;
+
+hAxes = 0.8;
+wAxes = 3.0;
+dh = 0.15;
+
+bm = 0.35;
+lm = 0.4;
+yllp = -0.23;
+
+f = figure;
+set(f,'Units','Inches','Position',[13,5,3.5,3.1]);
+
+ax1 = axes('Units','inches','Position',[lm,bm,wAxes,hAxes]);
+hold on;
+color = get(ax1,'ColorOrder');
+set(ax1,'FontName','Times','FontSize',8);
+plot(t,x_err_MEKF.p,'Color',color(1,:),'LineStyle','-');
+plot(t,x_err_MFG.p,'Color',color(2,:),'LineStyle','-');
+xlabel('time (second)','FontName','Times');
+yl = ylabel('pos err (m)','Units','inches');
+ylp = get(yl,'Position');
+ylp(1) = yllp;
+set(yl,'Position',ylp);
+
+ax2 = axes('Units','inches','Position',[lm,bm+hAxes+dh,wAxes,hAxes]);
+hold on;
+set(ax2,'FontName','Times','FontSize',8);
+plot(t,x_err_MEKF.v,'Color',color(1,:),'LineStyle','-');
+plot(t,x_err_MFG.v,'Color',color(2,:),'LineStyle','-');
+set(ax2,'XTick',[]);
+yl = ylabel('vel err (m/s)','Units','inches');
+ylp = get(yl,'Position');
+ylp(1) = yllp;
+set(yl,'Position',ylp);
+
+ax3 = axes('Units','inches','Position',[lm,bm+2*hAxes+2*dh,wAxes,hAxes]);
+hold on;
+set(ax3,'FontName','Times','FontSize',8);
+plot(t,x_err_MEKF.R*180/pi,'Color',color(1,:),'LineStyle','-');
+plot(t,x_err_MFG.R*180/pi,'Color',color(2,:),'LineStyle','-');
+set(ax3,'XTick',[]);
+ylim([0,180]);
+yticks([0,60,120,180])
+legend('EKF','MFG','Location','southeast');
+yl = ylabel('att err (deg)','Units','inches');
+ylp = get(yl,'Position');
+ylp(1) = yllp;
+set(yl,'Position',ylp);
+
+print('error','-depsc','-painters');
---
+++ Matlab/VIO-noMapNoise/plotKL.m
@@ -0,0 +1,28 @@
+close all;
+clear;
+
+load('KL.mat');
+
+hAxes = 0.8;
+wAxes = 3.0;
+dh = 0.15;
+
+bm = 0.35;
+lm = 0.4;
+yllp = -0.23;
+
+f = figure('Units','inches','Position',[13,5,3.5,1.2]);
+N = [4,6,8,10,12,14,16,18,20];
+hold on;
+plot(N,flip(KL(2,:)));
+plot(N,flip(KL(1,:)));
+xlim([4,20]);
+xticks(N);
+set(gca,'FontName','Times','FontSize',8);
+set(gca,'Units','Inches','Position',[lm,bm,wAxes,hAxes]);
+xlabel('Number of Points','FontName','Times');
+ylabel('KL Divergence','FontName','Times');
+legend('LM optimization','MFG');
+
+print('KL','-depsc','-painters');
+
