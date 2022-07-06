3dd71294e5b3c43705486520afebfed63255dc12
weixinwang <weixinwang442@gmail.com>
2022-07-06 13:49:52 -0400

    1, add KL when N=3 2, a singular case for pose

--- Matlab/VIO-noMapNoise/plotKL.m
+++ Matlab/VIO-noMapNoise/plotKL.m
@@ -12,11 +12,11 @@ lm = 0.4;
 yllp = -0.23;
 
 f = figure('Units','inches','Position',[13,5,3.5,1.2]);
-N = [4,6,8,10,12,14,16,18,20];
+N = [3,4,6,8,10,12,14,16,18,20];
 hold on;
 plot(N,flip(KL(2,:)));
 plot(N,flip(KL(1,:)));
-xlim([4,20]);
+xlim([3,20]);
 xticks(N);
 set(gca,'FontName','Times','FontSize',8);
 set(gca,'Units','Inches','Position',[lm,bm,wAxes,hAxes]);
